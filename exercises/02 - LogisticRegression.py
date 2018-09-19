0551774b3f5296c1e979069d5313cc1ce51ec069
Juliet Moreiro Bockhop <jmoreiro@microsoft.com>
2018-09-19 23:46:38 +0100

    Adding code, scripts and slides

---
+++ exercises/02 - LogisticRegression.py
@@ -0,0 +1,13 @@
+'''
+A very simple example of logistic regression via TensorFlow, used for the Deep Learning workshop.
+We will use the MNIST dataset of handwritten digits (http://yann.lecun.com/exdb/mnist/)
+
+Authors: Juliet Moreiro (@julietsvq) and Pablo Doval (@PabloDoval)
+'''
+
+import sys
+sys.path.append('../') 
+from utils.init import init
+
+if __name__ == '__main__':
+    init()
